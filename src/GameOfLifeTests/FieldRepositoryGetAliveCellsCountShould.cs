b5f26465bf55ab57233aadffb5426f11381963b0
Dmitry Afonchenko <undermove@yandex.ru>
2020-08-04 19:21:39 +0300

    add corner case

--- src/GameOfLifeTests/FieldRepositoryGetAliveCellsCountShould.cs
+++ src/GameOfLifeTests/FieldRepositoryGetAliveCellsCountShould.cs
@@ -42,5 +42,24 @@ namespace GameOfLifeTests
             
             Assert.AreEqual(7, aliveCellsCount);
         }
+        
+        [TestCase(0, 0)]
+        public void ReturnEightAliveNeighboursCount_WhenPointInLeftUpperCorner_AndOneDeadCellInNearPosition(
+            int deadCellRow, 
+            int deadCellColumn)
+        {
+            char[,] _field =
+            {
+                {'*', '*', '*'},
+                {'*', '*', '*'},
+                {'*', '*', '*'}
+            };
+            _field[deadCellRow, deadCellColumn] = '.'; 
+            var _repository = new FieldRepository(_field);
+            
+            var aliveCellsCount = _repository.GetAliveCellsCountForPosition(new Point(0,0));
+            
+            Assert.AreEqual(8, aliveCellsCount);
+        }
     }
 }
